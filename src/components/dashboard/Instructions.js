df02c0256ee201803e164689f546f2109ee66069
dwandrew <danwandrew@gmail.com>
2020-11-09 16:51:27 +0000

    css mods, change to background

--- src/components/dashboard/Instructions.js
+++ src/components/dashboard/Instructions.js
@@ -4,12 +4,18 @@ const Instructions = () => {
 
 return (<div>
 <h2>Instructions</h2>
+<div className= 'generation'>
 <h3>Generation of characters:</h3>
 <p>New characters can be generated via the New Chronicle button in the Navbar, they only require a Name to be be valid, but adding a desctription can help you really characterise the Immortal, once created you will be directed back to your List of Immortals. </p>
+</div>
+<div className= 'generation'>
 <h3>Adding traits to Immortals:</h3>
 <p>Click on the edit Immortal Button to add traits, then click it again to go back to your standard view</p>
+</div>
+<div className= 'generation'>
 <h3>Publishing Journals:</h3>
 <p>If you want to Publish the Journal of your Immortal Escapades, in the edit Immortal screen there is a publish journal Checkbox, this will allow all and sundry to enjoy your tale as it posts it to the front page</p>
+</div>
 </div>)
 
 }
